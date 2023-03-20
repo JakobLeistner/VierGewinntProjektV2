576f4f15adc2b151991cddbaf5457dd21165d0a3
JakobLeistner <jakob.leistner@infoteam.de>
2023-03-20 15:32:14 +0100

    Added Prove Of Concept files

---
+++ POC/SignalR_POC/SignalRClient/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.AspNetCore.SignalR.Client;
+
+Console.WriteLine("Hello, World!");
+var _hubConnection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:44362/fourwingamehub", (opts) =>
+                {
+                    opts.HttpMessageHandlerFactory = (message) =>
+                    {
+                        if (message is HttpClientHandler clientHandler)
+
+                            // always verify the SSL certificate in Debug
+                            clientHandler.ServerCertificateCustomValidationCallback +=
+                                (sender, certificate, chain, sslPolicyErrors) => { return true; };
+                        return message;
+                    };
+                })
+                .WithAutomaticReconnect()
+                .Build();
+Action<string> actionOnMessage = (msg) =>
+{
+    Console.WriteLine(msg);
+};
+_hubConnection.On("SendMessageToUser", actionOnMessage);
+try
+{
+    await _hubConnection.StartAsync();
+    Console.WriteLine(_hubConnection.ConnectionID);
+}
+catch (Exception ex)
+{
+
+    throw;
+}
+do
+{
+    await Task.Delay(200);
+} while (_hubConnection.State == HubConnectionState.Connecting);
+Console.WriteLine(_hubConnection.State.ToString());
+
+
+Console.ReadLine();
+
+
---
+++ POC/SignalR_POC/SignalR_POC/Hubs/TestHub.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalR_POC.Services;
+
+namespace SignalR_POC.Hubs
+{
+    public class TestHub : Hub
+    {
+        public TestHub(GameService gameService)
+        {
+            this.gameService = gameService;
+        }
+
+        private readonly GameService gameService;
+
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "SignalR Users");
+            gameService.connections.Add(Context.ConnectionId);
+            await base.OnConnectedAsync();
+            await DirectMessage("test");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        [HubMethodName("SendMessageToUser")]
+        public async Task DirectMessage(string message)
+        => await Clients.Client(gameService.connections[0]).SendAsync("SendMessageToUser", gameService.connections[0]);
+
+        [HubMethodName("BroadcastMessage")]
+        public async Task BroadcastMessage(string user, string message)
+        => await Clients.All.SendAsync("BroadcastMessage", user, message);
+    }
+}
