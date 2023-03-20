7482392f11434da1256fdd5cfe3ee56b1ec21832
JakobLeistner <jakob.leistner@infoteam.de>
2023-03-20 16:02:46 +0100

    Added Proof Of Concept files

--- POC/SignalR_POC/SignalR_POC/WeatherForecast.cs
+++ POC/SignalR_POC/SignalR_POC/WeatherForecast.cs
@@ -6,6 +6,8 @@ namespace SignalR_POC
 
         public int TemperatureC { get; set; }
 
+        public int TemperatureK => 273 + TemperatureC;
+
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
