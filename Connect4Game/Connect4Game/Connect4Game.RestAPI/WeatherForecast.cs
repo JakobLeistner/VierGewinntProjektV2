a60d5555d62487aea21c5ded68cf8620b7b4cd19
susha77 <alexandra.kaiser@infoteam.de>
2023-03-21 11:05:53 +0100

    Endlich API und Business Projekte hoffendlich richtig angelegt

--- Connect4Game/Connect4Game/Connect4Game.RestAPI/WeatherForecast.cs
+++ Connect4Game/Connect4Game/Connect4Game.RestAPI/WeatherForecast.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Connect4Game.RestAPI
 {
     public class WeatherForecast
@@ -8,6 +10,6 @@ namespace Connect4Game.RestAPI
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
     }
 }
